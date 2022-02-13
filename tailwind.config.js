4f4b31c609bf69cc9ef7457a2da144d1688cdfd5
Muhammed Mustafa <muhammed.mustafa.helal@gmail.com>
2022-02-13 18:44:39 +0100

    implemented Navbar and enabled darkmode with tailwind css throughout the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'md-invert': '0 4px 6px -1px rgba(255, 255, 255, 0.1), 0 4px 6px -2px rgba(255, 255, 255, 0.05)',
+      },
+    },
   },
   variants: {
-    extend: {},
+      boxShadow: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus', 'dark'],
   },
   plugins: [],
 }
